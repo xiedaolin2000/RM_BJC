9470136b6e6824a78954572946ac27166b8e0389
xiedaolin2000 <xiedaolin2000@163.com>
2018-06-06 23:51:20 +0800

    增加调薪，绩效模型，后台管理入口

--- HR/admin.py
+++ HR/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Person,Performace,PerformaceDetail,Salary
 
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(Performace)
+admin.site.register(PerformaceDetail)
+admin.site.register(Salary)
+
--- HR/models.py
+++ HR/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from datetime import date
 
+
 # Create your models here.
 
 class Person(models.Model):
@@ -12,13 +13,13 @@ class Person(models.Model):
     # staff = models.OneToOneField(auth.user, primary_key=True, verbose_name="员工姓名")
     #staff = models.OneToOneField(User, primary_key=True, verbose_name="员工姓名")
     #员工姓名
-    userName = models.CharField("姓名", max_length=15, default="无名氏")
+    userName = models.CharField("姓名", max_length=15, default="张三无名氏")
     #公司工号
-    workNo = models.CharField("公司工号", max_length=15)
+    workNo = models.CharField("公司工号", max_length=15,default="B-12345")
     #华为工号
     workNoExt = models.CharField("华为工号", max_length=15, null=True)
     #手机号码
-    mobilePhone = models.CharField("手机号码", max_length=15, default="00000000000")
+    mobilePhone = models.CharField("手机号码", max_length=15, default="18600000000")
     #身份证号码
     IDNo = models.CharField("身份证", max_length=18)
     #电子邮箱
@@ -30,12 +31,12 @@ class Person(models.Model):
     sex = models.CharField("性别", max_length=1, choices=(("0", "女"), ("1", "男")), default="1")
     # 岗位级别
     levelList = (
-        ("00", "00"),
-        ("1B", "1B"), ("1A", "1A"), ("2B", "2B"), ("2A", "2A"),
-        ("3B", "3B"), ("3A", "3A"), ("4B", "4B"), ("4A", "4A"),
-        ("5B", "5B"), ("5A", "5A"), ("6B", "6B"), ("6A", "6A"),
-        ("7B", "7B"), ("7A", "7A"), ("8B", "8B"), ("8A", "8A"),
-        ("9B", "9B"), ("9A", "9A"), ("10B", "10B"), ("10A", "10A"),
+        ("00",   "00"),
+        ("1B",   "1B"), ("1A",   "1A"), ("2B",   "2B"), ("2A",   "2A"),
+        ("3B",   "3B"), ("3A",   "3A"), ("4B",   "4B"), ("4A",   "4A"),
+        ("5B",   "5B"), ("5A",   "5A"), ("6B",   "6B"), ("6A",   "6A"),
+        ("7B",   "7B"), ("7A",   "7A"), ("8B",   "8B"), ("8A",   "8A"),
+        ("9B",   "9B"), ("9A",   "9A"), ("10B", "10B"), ("10A", "10A"),
         ("11B", "11B"), ("11A", "11A"), ("12B", "12B"), ("12A", "12A")
     )
     level = models.CharField("岗位级别", max_length=4, choices=levelList, default="00")
@@ -68,9 +69,12 @@ class Person(models.Model):
     profession = models.CharField("专业", max_length=20, null=True, default="")
     # 毕业时间
     graduatedDay = models.DateField("毕业日期", null=True,default=date.today)
+    
+    #人员在职状态
+    workStatus = models.CharField(verbose_name = "工作状态", max_length=5, 
+        choices=(('00','在职'), ('11','请假'), ('99','离职')), default="00", blank=False, null=False)
 
   
-
     def get_absolute_url(self):
         from django.urls import reverse
         # return reverse('PersonListView', kwargs={'pk': self.pk})
@@ -84,21 +88,69 @@ class Person(models.Model):
 #从公司的花名册表单中读取员工数据，更新到数据库中
 def import_data_excel(fileName):
     pass
+#薪资调整记录
 class Salary(models.Model):
     """
     员工薪资调整的详细数据
     """
     #员工
     person = models.ForeignKey(Person,on_delete=models.CASCADE , verbose_name="员工")
-    #调薪日期
-    adjustDate = models.DateField("调薪日期", default=date.today)
+    #调薪生效日期，也就是说薪资是在几月份进行生效的
+    applyDate = models.DateField("生效月份", default=date.today)
     #调薪幅度
     adjustRange = models.IntegerField("调薪幅度", default=0)
     #调整后薪资
     salaryFinal = models.IntegerField("调整后薪资", default=0)
+    #调薪日期
+    adjustDate = models.DateField("调薪日期", default=date.today)
     #备注
     notes = models.CharField("备注", max_length=200, blank=True, null=True)
     #此处表示表名字
     verbose_name = "薪资调整数据"
     def __str__(self):
         return self.person
+
+#员工绩效考评记录
+class Performace(models.Model):
+    """
+    员工绩效考核表
+    Employee performance evaluation review
+    """
+    #关联员工表数据，多对一 ；外键要定义在‘多’的一方
+    person     = models.ForeignKey(Person,on_delete=models.CASCADE, verbose_name="员工")
+
+    #绩效考察周期
+    dateRange  = models.CharField(verbose_name ="考核周期", max_length=10, default="2018Q1", blank=True, null=True)  
+
+    #绩效得分    
+    total      = models.IntegerField(verbose_name ="绩效得分",default=0,blank=True, null=True)
+
+    #绩效结果
+    result     = models.CharField(verbose_name ="绩效结果", max_length=10, blank=True, null=True)
+
+    #绩效考评人
+    appraisers = models.CharField(verbose_name ="考评人", max_length=10, blank=True, null=True)
+
+    #备注
+    notes      = models.CharField(verbose_name = "备注", max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return ""
+
+class PerformaceDetail(models.Model):
+    """
+    员工绩效考评明细项记录表
+    """
+
+    #关联员工表数据，多对一 ；外键要定义在‘多’的一方
+    person     = models.ForeignKey(Person,on_delete=models.CASCADE, verbose_name="员工")
+    #绩效考察周期
+    dateRange  = models.CharField(verbose_name ="考核周期", max_length=10, default="2018Q1", blank=True, null=True)
+    
+    #KPI的指标项
+    kpi = models.CharField(verbose_name ="考核项", max_length=50, default="考核指标项内容")
+    kpiValue= models.DecimalField(verbose_name="考核项得分", max_digits=5, decimal_places=2, default=0.00)
+
+
+    def __str__(self):
+        return ""
